fa0f6e3bb638484277178cd8b9d336a08e2def5c
Anushiya <anushiya.kokila@gmail.com>
2025-01-10 08:49:03 +0530

    add file

---
+++ javascript/datatypes.js
@@ -0,0 +1,16 @@
+a="SECE"
+console.log("A"+a)
+b=true
+console.log("b"+b)
+console.log("type is"+typeof(a))
+
+array=['a',10,true,[20,30],"Sri Eshwar"]
+console.log(array)
+
+obj={
+    firstname:"Sri",
+    lastname:"eshwar"
+}
+console.log(obj)
+console.log(typeof(obj))
+

1a45d4b96ac27e3b4131f1cdb69bc5c92b2cdc66
Anushiya <anushiya.kokila@gmail.com>
2025-01-10 14:00:06 +0530

    files added

--- javascript/operator.js
+++ javascript/operator.js
@@ -2,3 +2,13 @@ var a =5
 var b ='5'
 console.log(a==b)
 console.log(a===b)//check types
+
+//bitwise operators
+var c=10
+var d=2
+console.log(~c)
+console.log(c&d)
+console.log(c|d)
+console.log(c>>1)
+console.log(c<<1)
+console.log(c^d)

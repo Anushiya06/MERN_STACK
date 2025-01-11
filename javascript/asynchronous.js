1f95d1ac78a365ad9d1197f4f5ed8b8e49aec5f4
Anushiya <anushiya.kokila@gmail.com>
2025-01-11 12:14:31 +0530

    add file

--- javascript/asynchronous.js
+++ javascript/asynchronous.js
@@ -11,3 +11,33 @@ asAw().then(function(output){
     console.log(output);
 })
 
+likeCode=async()=>{
+    return new Promise((likingPost)=>{
+        setTimeout(()=>{
+        likingPost("Liked the post created")
+       },2000)
+    })
+}
+commentCode=async()=>{
+    return new Promise((commentingPost)=>{
+        setTimeout(()=>{
+        commentingPost("Comment posted to the post created")
+       },5000);
+    });
+}
+
+async function createPost(){
+    var post=new Promise((cPost)=>{
+        setTimeout(()=>{
+            cPost("Post created successfully")
+
+    },1000);
+
+   });
+   
+   var[post,like,comment]=await Promise.all([post,likeCode(),commentCode()])
+   console.log(await post);
+   console.log(await likeCode());
+   console.log(await commentCode());
+}
+createPost()

513480da164ff9962481cb374378f8f7f11e2d0f
Anushiya06 <153485488+Anushiya06@users.noreply.github.com>
2025-01-21 21:48:24 +0530

    day 8 AN task

--- src/assets/Functional Component/App.jsx
+++ src/assets/Functional Component/App.jsx
@@ -1,50 +1,27 @@
-/*import Home from './assets/Functional Component/Home'
-import About from './assets/Functional Component/About'
-import Gallery from './assets/Functional Component/gallery'
-import ClassCompEG from './assets/Class Component/ClassCompEG'
 import './App.css'
+import Home from './assets/Functional Component/Home';
+import About from './assets/FunctionalComponents/About';
+import Gallery from './assets/FunctionalComponents/gallery';
+import Contact from './assets/FunctionalComponents/Contact';
+import Navbar from './assets/FunctionalComponents/Navbar';
+import Signup from './assets/FunctionalComponents/Signup';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <div>
-        <Home/>
-      <About/>
-      <ClassCompEG/>
-      <Gallery page="gallery" img="SECE logo"/>
-      <Gallery page="gallery" img="SECE logo"/>
-      </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} ></Route>
+      <Route path="/about" element={<About />} ></Route>
+      <Route path="/gallery" element={<Gallery page="Gallery" img ="time"/>} ></Route>
+      <Route path="/contact" element={<Contact />} ></Route>
+      <Route path="/signup" element={<Signup />} ></Route>
+     </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
-export default App */
-
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import About from './components/FunctionalComponents/About';
-import ClassCompEG from './assets/Class Component/ClassCompEG'
-import Gallery from './components/FunctionalComponents/Gallery';
-import Home from './components/FunctionalComponents/Home';
-import Contact from './components/FunctionalComponents/Contact';
-import Navbar from './components/FunctionalComponents/Navbar';
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-      <Route path="/"element={<Home />}></Route>
-      <Route path="/about"element={<About />}></Route>
-      <Route path="/gallery"element={<Gallery page="Gallery" image="SECE Logo" />}></Route>
-      <Route path="/contact"element={<Contact />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-export default App;
-
+export default App
